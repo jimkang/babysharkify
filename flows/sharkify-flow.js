54bb807aa863a15d9b084f0dcbb86e0c8b8372b3
Jim Kang <jimkang@gmail.com>
2019-04-13 22:51:27 -0400

    Somewhat singing-like.

--- flows/sharkify-flow.js
+++ flows/sharkify-flow.js
@@ -5,13 +5,14 @@ var CollectCtor = require('collect-in-channel');
 var curry = require('lodash.curry');
 var handleError = require('handle-error-web');
 var bodyMover = require('request-body-mover');
-var queue = require('d3-queue').queue;
+var findWhere = require('lodash.findwhere');
 
 var config = require('../config');
 var synth = window.speechSynthesis;
 
+var pitches = [0.0, 0.5, 2.0];
+
 function sharkifyFlow({ text }) {
-  console.log('text', text);
   var channel = {
     text,
     wordworkerKey: config.wordworker.key
@@ -59,34 +60,41 @@ function singIt({ syllablesGroupedByWord }, done) {
   // use the IPA or Arpabet representation of the syllables.
   // Instead, we have to use whole words that sound sort
   // of like each syllable.
-  var q = queue(1);
   syllablesGroupedByWord.wordGuesses
     .map(guesses => guesses[0])
     .forEach(queueSyllable);
-  q.awaitAll(done);
 
-  function queueSyllable(wordApproximation) {
-    q.defer(speakSyllable, { wordApproximation });
+  function queueSyllable(wordApproximation, i) {
+    setTimeout(callSpeak, i * 500);
+    function callSpeak() {
+      speakSyllable({ wordApproximation, pitch: getPitchForSequence(i) });
+    }
   }
 }
 
-function speakSyllable({ wordApproximation }, done) {
+function getPitchForSequence(i) {
+  return pitches[i];
+}
+
+function speakSyllable({ wordApproximation, pitch }) {
   console.log('speaking syllable:', wordApproximation);
   var utterThis = new SpeechSynthesisUtterance(wordApproximation);
   utterThis.onend = onEnd;
   utterThis.onerror = onError;
-  utterThis.voice = synth.getVoices()[0];
-  //utterThis.pitch = pitch.value;
+  console.log(synth.getVoices());
+  utterThis.voice = findWhere(synth.getVoices(), { lang: 'en-US' });
+  utterThis.pitch = pitch;
   //utterThis.rate = rate.value;
   synth.speak(utterThis);
 
   function onEnd() {
-    done();
+    //done();
+    console.log('Done speaking', wordApproximation);
   }
 
   function onError(e) {
     console.log('onError', e);
-    done(new Error('SpeechSynthesisUtterance.onerror'));
+    //done(new Error('SpeechSynthesisUtterance.onerror'));
   }
 }
 
